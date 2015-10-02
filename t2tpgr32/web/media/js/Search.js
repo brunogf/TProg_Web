c0f3b8f624e1f695573a331dd2b8ede4997886d0
Nicolas Rostan <nicolas.rostan@fing.edu.uy>
2015-10-02 15:20:02 -0300

    ---

--- t2tpgr32/web/media/js/Search.js
+++ t2tpgr32/web/media/js/Search.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-$(document.getElementById("form-busqueda")).on("keyup", "#criterio", function() {  // When HTML DOM "keyup" event is invoked on element with ID "lala", execute the following function...   
-    $.get("AjaxRequest",{criterio : $("#criterio").val()} ,function(responseJson) {    // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response JSON...
+$(document.getElementById("form-busqueda")).on("keyup", "#criterio", function() {  
+    $.get("AjaxRequest",{criterio : $("#criterio").val()} ,function(responseJson) {  
                 var arr =[];
-                $.each(responseJson, function(index, item) { // Iterate over the JSON array.
-                    arr[index] = item;      // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
+                $.each(responseJson, function(index, item) { 
+                    arr[index] = item;      
                 });
                 $("#criterio").autocomplete({source: arr});
         });
