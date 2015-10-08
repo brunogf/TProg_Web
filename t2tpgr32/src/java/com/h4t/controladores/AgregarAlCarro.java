8d4caf3b758c5d16224a0b4956b3bd1d48d232b5
piñe <pie@JuanDalto>
2015-10-08 16:58:09 -0300

    Modifique AgregarAlCarro

--- t2tpgr32/src/java/com/h4t/controladores/AgregarAlCarro.java
+++ t2tpgr32/src/java/com/h4t/controladores/AgregarAlCarro.java
@@ -7,14 +7,22 @@ package com.h4t.controladores;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import tpgr32.*;
+import java.util.regex.*;
 
 /**
  *
@@ -33,17 +41,40 @@ public class AgregarAlCarro extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ParseException {
         
          response.setContentType("text/html;charset=UTF-8");
-         Set<ParDPD> carro;
+         PrintWriter out = response.getWriter();
          if(request.getSession().getAttribute("publicaciones-carro") == null){
-             carro = new HashSet<ParDPD>();
+             Set<ParDPD> carro = new HashSet<ParDPD>();
              request.getSession().setAttribute("publicaciones-carro", request);
-             
          }
+         FabricaControladores fab = FabricaControladores.getInstancia();
+         IControladorPublicacion pub = fab.getControladorPublicacion();
+         DataPublicacion dp = pub.infoServicio(request.getParameter("publicacion"),request.getParameter("proveedor"));
+         try{
+         String fechaini = request.getParameter("fechaini").toString();
+         String fechafin = request.getParameter("fechafin").toString();
+         String datosfechaini[] = fechaini.split("[/]");
+         String datosfechafin[] = fechafin.split("[/]");
+         
+         Date fechainicial =  new Date(Integer.parseInt(datosfechaini[2]),Integer.parseInt(datosfechaini[1])
+                               ,Integer.parseInt(datosfechaini[0]));
+         Date fechafinal =  new Date(Integer.parseInt(datosfechafin[2]),Integer.parseInt(datosfechafin[1])
+                               ,Integer.parseInt(datosfechafin[0]));
+         DataDisponibilidad dd = new DataDisponibilidad(Integer.parseInt(request.getParameter("cantidad")),
+                                     fechainicial,fechafinal);
+         
+         ParDPD par = new ParDPD(dp,dd);
+         
+         }
+         catch(Exception e){
+            out.print("EXCEPCION!");
+        }
+         
+         
         
-       
+     
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -58,7 +89,11 @@ public class AgregarAlCarro extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(AgregarAlCarro.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -72,7 +107,11 @@ public class AgregarAlCarro extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(AgregarAlCarro.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -84,5 +123,6 @@ public class AgregarAlCarro extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
 
 }
